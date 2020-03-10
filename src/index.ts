9d410cedcb5379b45a9d2967de6d684809963fa4
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-03-11 00:04:05 +0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import {
 (() => {
   let audioCtx = new AudioContext();
   let source: AudioBuffer = null;
+  let node: any = null;
   window.addEventListener("load", () => {
-    const button = document.body.querySelector("#buttonStart");
+    const button: HTMLElement = document.body.querySelector("#buttonStart");
     button.addEventListener("click", () => {
       // autoplay policy
       if (audioCtx.state === "suspended") {
@@ -15,14 +16,25 @@ import {
       }
 
       if (source) {
-        let node = new AudioBufferSourceNode(audioCtx, { buffer: source });
-        node.connect(audioCtx.destination);
-        node.addEventListener("ended", () => {
-          node.stop();
-          node.disconnect();
-          node = null;
-        });
-        node.start();
+        if (button.dataset.playing === "false") {
+          if (node) {
+            node.start();
+          } else {
+            node = new AudioBufferSourceNode(audioCtx, { buffer: source });
+            node.connect(audioCtx.destination);
+            node.addEventListener("ended", () => {
+              node.stop();
+              node.disconnect();
+              node = null;
+              button.dataset.playing = "false";
+            });
+            node.start();
+          }
+          button.dataset.playing = "true";
+        } else if (button.dataset.playing === "true") {
+          audioCtx.suspend();
+          button.dataset.playing = "false";
+        }
       } else {
         // NOTE: need loading flag
         fetch("7sxtEOR7zhrd-60sec-fade-out.128.mp3")
@@ -34,14 +46,25 @@ import {
           })
           .then(decodeAudio => {
             source = decodeAudio;
-            let node = new AudioBufferSourceNode(audioCtx, { buffer: source });
-            node.connect(audioCtx.destination);
-            node.addEventListener("ended", () => {
-              node.stop();
-              node.disconnect();
-              node = null;
-            });
-            node.start();
+            if (button.dataset.playing === "false") {
+              if (node) {
+                node.start();
+              } else {
+                node = new AudioBufferSourceNode(audioCtx, { buffer: source });
+                node.connect(audioCtx.destination);
+                node.addEventListener("ended", () => {
+                  node.stop();
+                  node.disconnect();
+                  node = null;
+                  button.dataset.playing = "false";
+                });
+                node.start();
+              }
+              button.dataset.playing = "true";
+            } else if (button.dataset.playing === "true") {
+              audioCtx.suspend();
+              button.dataset.playing = "false";
+            }
           })
           .catch(error => {
             console.error(error);
