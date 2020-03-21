4d3675104fcace00ab7a8dd061bf8cce97f573e0
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-03-21 17:43:19 +0700

    feat(draw): draw method

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,12 @@ import {
   function visualize() {
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-    analyser.getByteTimeDomainData(dataArray);
+
+    function draw() {
+      // drawVisual
+      requestAnimationFrame(draw);
+      analyser.getByteTimeDomainData(dataArray);
+    }
+    draw();
   }
 })();
