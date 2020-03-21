b531f706914cde83ed1535232dc08dc95a4760d0
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-03-21 19:21:36 +0700

    chore(bgmnode): prevent null access

--- src/index.ts
+++ src/index.ts
@@ -62,9 +62,11 @@ import {
       analyser.connect(audioCtx.destination);
       visualize();
       bgmNode.addEventListener("ended", () => {
-        bgmNode.stop();
-        bgmNode.disconnect();
-        bgmNode = null;
+        if (bgmNode) {
+          bgmNode.stop();
+          bgmNode.disconnect();
+          bgmNode = null;
+        }
       });
       bgmNode.start();
     }
