d4f588894efce7f019294bb7531a799aaa100f18
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-03-21 18:19:16 +0700

    feat(visualizer): set visualizer example
    https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API

--- src/index.ts
+++ src/index.ts
@@ -82,6 +82,32 @@ import {
       // drawVisual
       requestAnimationFrame(draw);
       analyser.getByteTimeDomainData(dataArray);
+      canvasCtx.fillStyle = "rgb(200, 200, 200)";
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+      canvasCtx.beginPath();
+
+      const sliceWidth = (WIDTH * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = (v * HEIGHT) / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvasElement.width, canvasElement.height / 2);
+      canvasCtx.stroke();
     }
     draw();
   }
